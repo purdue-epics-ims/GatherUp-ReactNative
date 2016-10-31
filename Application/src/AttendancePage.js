876b96dbf68e31680e0f1ac7253ae1d0ac504908
nickm5295 <herreran@purdue.edu>
2016-10-31 19:29:54 -0400

    Added firebase functionality to AttendancePage

--- Application/src/AttendancePage.js
+++ Application/src/AttendancePage.js
@@ -8,10 +8,28 @@ import {
 } from'react-native';
 
 import Firebase from 'firebase';
-//import LinearGradient from 'react-native-linear-gradient';
 
+const firebaseConfig = {
+  apiKey: "<your-api-key>",
+  authDomain: "<your-auth-domain>",
+  databaseURL: "<your-database-url>",
+  storageBucket: "<your-storage-bucket>",,
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 export default class AttendancePage extends Component {
+
+	constructor(props) {
+    super(props);
+    this.state = {
+      puidString: '',
+      firstNameString: '',
+	  lastNameString: '',
+	  emailString: ''
+    };
+
+	{/*this.itemsRef = firebaseApp.database().ref();
+	}*/}
 
   render() {
     return (
@@ -24,6 +42,7 @@ export default class AttendancePage extends Component {
           secureTextEntry = {true}
           placeholder = "PUID"
           placeholderTextColor = "black"
+		  onChangeText = {(text) => {this.setState({puidString: text})}}
         />
         <Text>
           Or:
@@ -32,16 +51,19 @@ export default class AttendancePage extends Component {
           style={{width: 150, textAlign: 'center', alignItems: 'center',}}
           placeholder = "First Name"
           placeholderTextColor = "black"
+		  onChangeText = {(text) => {this.setState({firstNameString: text})}}
         />
         <TextInput
           style={{width: 150, textAlign: 'center', alignItems: 'center',}}
           placeholder = "Last Name"
           placeholderTextColor = "black"
+		  onChangeText = {(text) => {this.setState({lastNameString: text})}}
         />
         <TextInput
           style={{width: 150, textAlign: 'center', alignItems: 'center',}}
           placeholder = "Email"
           placeholderTextColor = "black"
+		  onChangeText = {(text) => {this.setState({emailString: text})}}
         />
 
         <View style={styles.submitbutton}>
