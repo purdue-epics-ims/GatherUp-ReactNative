3d91bfdb66a875cc16665dc39d59aa8fce1e2879
Nikhil Nand Kumar <nikhilfaips@gmail.com>
2017-02-15 20:54:34 -0500

    Modified index file to run for iOS

--- Application/index.ios.js
+++ Application/index.ios.js
@@ -9,27 +9,91 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+	Navigator
 } from 'react-native';
 
-import Main from './src/LoginPage';
+import AttendancePage from './src/AttendancePage';
+import LoginFields from './src/LoginPage';
+import EventListPage from './src/EventListPage';
 import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBpBEVFlT7PpcPq3ZA_Yj0U6Lq1vQfvq0c",
+  authDomain: "dazzling-inferno-9963.firebaseapp.com",
+  databaseURL: "https://dazzling-inferno-9963.firebaseio.com",
+  storageBucket: "gs://dazzling-inferno-9963.appspot.com",
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig, 'MainFirebase');
 
 class Application extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+		const routes = [
+      {title: 'Login Page', index: 0},
+      {title: 'Event List', index: 1},
+      {title: 'Attendance Page', index: 2}
+    ];
+
+		return (
+      <Navigator
+        initialRoute={routes[0]}
+        initialRouteStack={routes}
+        configureScene={() => {
+          return Navigator.SceneConfigs.FloatFromRight;
+        }}
+        renderScene={(route, navigator) => {
+          const routeTitle = route.title;
+          if (routeTitle === 'Login Page') {
+            return (
+              <LoginFields
+                firebaseApp={firebaseApp}
+                title={routes[0].title}
+                onForward={() => {
+                  const nextIndex = route.index + 2;
+                  navigator.push({
+                    title: routes[nextIndex].title,
+                    index: nextIndex
+                  });
+                }}
+
+                onBack={() => {
+                  if (route.index > 0) {
+                    navigator.pop();
+                  }
+                }}
+                />
+            );
+          }
+          else if (routeTitle === 'Event List') {
+           return (
+             <EventListPage
+               firebaseApp={firebaseApp}
+               title={routes[1].title}
+                onForward={()=> {
+                 const nextIndex = route.index + 1;
+                 navigator.push({
+                   title: routes[nextIndex].title,
+                   index: nextIndex,
+                   passProps: {
+                   }
+                 });
+               }}
+
+               onBack={() => {
+                 if (route.index > 0) {
+                   navigator.pop();
+                 }
+               }}
+                />
+            );
+          }
+          else if (routeTitle === 'Attendance Page') {
+            return (
+              <AttendancePage />
+            );
+          }
+        }}
+      />
     );
   }
 }
