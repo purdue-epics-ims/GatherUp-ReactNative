da1a1fbf837f57fd0e2641860355f3fb05f9c8ac
kmada97 <kalyanmada97@gmail.com>
2017-10-27 11:05:33 -0400

    Initial additions for bridge to native from IOS

---
+++ UniMag_Bridge/UniMagImport.h
@@ -0,0 +1,23 @@
+//import RCTBridgeModule
+#if __has_include(<React/RCTBridgeModule.h>)
+#import <React/RCTBridgeModule.h>
+#elif __has_include("RCTBridgeModule.h")
+#import "RCTBridgeModule.h"
+#else
+#import "React/RCTBridgeModule.h"
+#endif
+
+
+#import --actual sdk . h --
+
+@interface UniMagImport : NSObject <RCTBridgeModule>
+
+@property (nonatomic, strong)  
+@property (nonatomic, strong)
+@property (nonatomic, strong)
+@property (nonatomic, strong)
+@property (nonatomic, strong)
+
+
+
+@end
---
+++ UniMag_Bridge/UniMagImport.m
@@ -0,0 +1,36 @@
+#import "UniMagImport.h"
+@implementation UniMagImport
+RCT_EXPORT_MODULE();
+RCT_EXPORT_METHOD(checkConnection){
+		
+}
+
+RCT_EXPORT_METHOD(checkRead: (NSString *) name, location: (NSString *)location){
+
+}
+
+RCT_EXPORT_METHOD(checkID: (NSString *) name, location:(NSString *)location){
+
+}
+
+RCT_EXPORT_METHOD(checkErr){
+
+}
+
+RCT_EXPORT_METHOD(checkreader){ 
+
+}
+
+RCT_EXPORT_METHOD( ){
+
+}
+
+RCT_EXPORT_METHOD( ){
+
+}
+
+RCT_EXPORT_METHOD( ){
+
+}
+
+@end
